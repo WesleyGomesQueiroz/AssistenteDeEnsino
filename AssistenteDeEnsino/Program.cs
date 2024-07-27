a2620e4b6ddb898331de2fdf3a186ec4ec67b436
wesley_gomes_queiroz <wesleygomesqueiroz@gmail.com>
2024-07-27 16:22:43 -0300

    Add project files.

---
+++ AssistenteDeEnsino/Program.cs
@@ -0,0 +1,37 @@
+using AssistenteDeEnsino.Components;
+using AssistenteDeEnsino.Components.Player.Data;
+using AssistenteDeEnsino.Configurations;
+using AssistenteDeEnsino.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IVideoRepository, VideoRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.UseDbMigrationHelper();
+app.Run();
